2d943a4bd8440c3bf1199c719ee725ab3d9bb777
dablu13 <muzakifarhan12@yahoo.com>
2017-10-02 15:56:43 +0700

    Menambhakan Cetak Garis Baru

---
+++ src/latihan2oktober2017/Latihan2Oktober2017.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2oktober2017;
+
+/**
+ *
+ * @author user
+ */
+public class Latihan2Oktober2017 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println();
+    }
+    
+}
