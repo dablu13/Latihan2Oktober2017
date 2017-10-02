55db1cde17b7e946b5ce201ebfb7943e6346b3e3
dablu13 <muzakifarhan12@yahoo.com>
2017-10-02 16:31:24 +0700

    Baru

--- src/latihan2oktober2017/Ya.java
+++ src/latihan2oktober2017/Ya.java
@@ -10,5 +10,9 @@ package latihan2oktober2017;
  * @author user
  */
 public class Ya {
-    
+ 
+    public static void main(String[] args) {
+        System.out.println("Ya aku");
+        System.out.println("Ya kamu");
+    }
 }
