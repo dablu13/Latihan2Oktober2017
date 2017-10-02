553967d1fb7d79ca556c9dc5b8ee13b26df6f44e
dablu13 <muzakifarhan12@yahoo.com>
2017-10-02 16:13:16 +0700

    Waw

--- src/latihan2oktober2017/Latihan2Oktober2017.java
+++ src/latihan2oktober2017/Latihan2Oktober2017.java
@@ -17,6 +17,7 @@ public class Latihan2Oktober2017 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println();
+        System.out.print(2.0);
     }
     
 }
